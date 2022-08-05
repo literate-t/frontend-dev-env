1fcd232313e1e26258c5d3c7660c9d43686cea5c
literate <gaekoon@gmail.com>
2022-08-05 14:55:23 +0900

    add: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,13 @@
-// TODO: eslint를 구성해 보세요. 
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
