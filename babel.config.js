a9ab8cbf7bd163c5d8af36080c42faf61ffce812
literate <gaekoon@gmail.com>
2022-08-05 12:26:13 +0900

    add: sass-loader 추가

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         targets: {
-          chrome: "79",
-          ie: "11" // npm i regenerator-runtime 필요함
+          ie: 11, // npm i regenerator-runtime 필요함
         },
-        useBuiltIns: "usage", // 폴리필 사용 방식 지정
+        useBuiltIns: 'usage', // 폴리필 사용 방식 지정
         corejs: {
           // 폴리필 버전 지정
-          version: 2
-        }
-      }
-    ]
-  ]
+          version: 3,
+        },
+      },
+    ],
+  ],
 };
