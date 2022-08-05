9a4151875a9296c1ca3027c290666fbdea37aa40
literate <gaekoon@gmail.com>
2022-08-05 12:09:34 +0900

    add: babel-preset 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,73 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    main: "./src/app.js"
+    main: './src/app.js',
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve("./dist")
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+    assetModuleFilename: '[name].[ext]?[hash]',
   },
   module: {
     rules: [
       {
         test: /\.(scss|css)$/,
         use: [
-          process.env.NODE_ENV === "production"
+          process.env.NODE_ENV === 'production'
             ? MiniCssExtractPlugin.loader // 프로덕션 환경
-            : "style-loader", // 개발 환경
-          "css-loader"
-        ]
+            : 'style-loader', // 개발 환경
+          'css-loader',
+        ],
       },
       {
-        test: /\.(png|jpg|svg|gif)$/,
-        loader: "url-loader",
-        options: {
-          name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
-        }
-      }
+        test: /\.(png|jpg|svg|gif)/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10000,
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
       /**
        * TODO: babel-loader를 구성해 보세요.
        */
-    ]
+    ],
   },
   plugins: [
     new webpack.BannerPlugin({
-      banner: `빌드 날짜: ${new Date().toLocaleString()}`
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`,
     }),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
       templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : '',
       },
       minify:
-        process.env.NODE_ENV === "production"
+        process.env.NODE_ENV === 'production'
           ? {
               collapseWhitespace: true, // 빈칸 제거
-              removeComments: true // 주석 제거
+              removeComments: true, // 주석 제거
             }
           : false,
-      hash: process.env.NODE_ENV === "production"
+      hash: process.env.NODE_ENV === 'production',
     }),
     new CleanWebpackPlugin(),
-    ...(process.env.NODE_ENV === "production"
+    ...(process.env.NODE_ENV === 'production'
       ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
-      : [])
-  ]
+      : []),
+  ],
 };
